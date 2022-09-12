af87b9a7f00eb5ab72c544615c3a23faafcb36e1
Alejandro Tomasella <aletomasella09@gmail.com>
2022-09-12 00:39:24 -0300

    Adding Priority Queues

---
+++ Priority Queue/PrioritiesQueues.js
@@ -0,0 +1,13 @@
+// PRIORITY QUEUE
+// A priority queue is a data structure where each element has a priority.
+// Elements with higher priorities are served before elements with lower priorities.
+//
+
+// HEAP
+// A heap is a tree-based data structure that satisfies the heap property:
+// 1 If A is a parent node of B then key(A) is ordered with respect to key(B) with the same ordering applying across the heap.
+// 2 A heap can be classified further as either a "max heap" or a "min heap".
+// 3 In a max heap, the key of a parent node is always greater than or equal to the keys of its children.
+// 4 In a min heap, the key of a parent node is always less than or equal to the keys of its children.
+
+// COMPLEXITY
