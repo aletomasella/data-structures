49fd1278b2bb5f797563ebd47fb9c672273f6708
aletomasella <aletomasella09@gmail.com>
2022-09-19 13:32:43 -0300

    Adding QuickSort

--- Algorithms/QuickSort.ts
+++ Algorithms/QuickSort.ts
@@ -1,6 +1,12 @@
 // QUICKSORT ALGORITHM
 
-const qs = (arr: number[], low: number, high: number) => {};
+const qs = (arr: number[], low: number, high: number) => {
+  if (low >= high) return;
+
+  const pivotIndex = partition(arr, low, high);
+  qs(arr, low, pivotIndex - 1);
+  qs(arr, pivotIndex + 1, high);
+};
 
 const partition = (arr: number[], low: number, high: number) => {
   const pivot = arr[Math.floor((low + high) / 2)];
@@ -13,13 +19,29 @@ const partition = (arr: number[], low: number, high: number) => {
       arr[i] = temp;
     }
   }
-  const temp = arr[index + 1];
+  index++;
+  arr[high] = arr[index];
+  arr[index] = pivot;
+  return index;
 };
 
 function quickSort(arr: number[]): number[] {
-  if (arr.length <= 1) return arr;
-  const pivot = Math.floor(arr.length / 2);
-  return [];
+  qs(arr, 0, arr.length - 1);
+  return arr;
 }
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array = [
+  2, 54, 56, 745, 6, 23, 4, 234, 6, 78, 34, 23, 1234, 4, 5, 123, 45, 5, 6, 77,
+  234, 4, 656, 2,
+];
+const array2 = [
+  10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 3, 4, 5, 67, 4, 43, 23, 4, 6, 7, 8, 345, 1,
+  4234, 5234, 556, 5,
+];
+
+// Complexity Analysis
+// Time complexity: O(n log n) to O(n^2)
+// Space complexity: O(log n)
+
+console.log(quickSort(array));
+console.log(quickSort(array2));
